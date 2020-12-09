e19434ab0b41ac90a3061236676e50347fe4d973
macwis <mw@iceam.com>
2020-12-09 16:02:10 +0100

    Clean-up and semnatic ui support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header, Icon } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header as="h2" icon>
+          <Icon name="settings" />
+          Congratulations!
+          <Header.Subheader>
+            You are running custom React dev environment with Docker, Semantic UI and Django API.
+          </Header.Subheader>
+        </Header>
       </header>
     </div>
   );
